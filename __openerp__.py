acab240d5cf87c8b6d07389bc2eb4c0e9b99a66e
luckysher <luckyshera95@gmail.com>
2017-08-22 10:04:09 +0530

    updated settings file

--- __openerp__.py
+++ __openerp__.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Goods-Management",
-
-    'summary': """
-        Managing Goods Simple""",
-
+    'version': "1.1",
+    'summary': """Managing Goods Simple""",
     'description': """
         The goods management app can help the users the manage their goods via orders, bills, invoices and
         tracking of goods etc.
-    """,
-
+     """,
     'author': "luckyshera",
     'website': "http://www.luckyshera.com",
     'category': 'Uncategorized',
     'version': '0.1',
     'depends': ['base'],
-
     'data': [
         'security/ir.model.access.csv',
         'templates.xml',
@@ -23,4 +19,7 @@
     'demo': [
         'demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
