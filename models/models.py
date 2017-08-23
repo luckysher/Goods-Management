430ec5a57aba1f2004fe40fa736885d014a2587c
luckysher <luckyshera95@gmail.com>
2017-08-24 02:22:26 +0530

    added model for supplier

--- models/models.py
+++ models/models.py
@@ -10,4 +10,18 @@ class goods(osv.osv):
         'description': fields.text('Description', help="Detailed information regarding the good", select=True, copy=True)
     }
 
+class goods_supplier(osv.osv):
+    _name = 'goods.supplier'
+    _table = 'goods_supplier'
+    _description = 'Goods suppliers'
+    _columns = {
+        'name': fields.many2one('res.users', help='Supplier that will supplier the goods', select=True, copy=False),
+        'address': fields.char('Supplier address', help='Supplier address from where goods, order will be received and send', select=True),
+        'company_name': fields.char('Company name', help='Supplier`s company name', select=True),
+        'email': fields.char('Email', help='Email Id of supplier', select=True),
+        'website': fields.char('Website', help='Supplier`s website', select=True),
+        'mobile': fields.char('Mobile No', help='Supplier`s mobile number', select=True),
+        'fax': fields.char('Fax', help='Supplier`s Fax', select=True)
+
+    }
 
