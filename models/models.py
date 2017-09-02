1b873d2d35de5422c08fb3e73f59ef1cb2f5dd9d
luckysher <luckyshera95@gmail.com>
2017-09-02 17:33:10 +0530

    updated goods model

--- models/models.py
+++ models/models.py
@@ -1,18 +1,19 @@
 from openerp.osv import fields, osv
 
-class goods(osv.osv):
+class good(osv.osv):
 
-    _name = 'gdm.goods'
-    _table = 'gdm_goods'
+    _name = 'gdm.good'
+    _table = 'gdm_good'
     _description = 'Goods to send or receive'
     _columns = {
         'name': fields.char('Name', help="Name of the goods that will either supplied or received", select=True, copy=True),
-        'description': fields.text('Description', help="Detailed information regarding the good", select=True, copy=True)
+        'desc': fields.text('Description', help="Detailed information regarding the good", select=True, copy=True),
+        'mf_date': fields.date('Manufacturing date', help="Manufatring date of this good")
     }
 
 class goods_supplier(osv.osv):
-    _name = 'goods.supplier'
-    _table = 'goods_supplier'
+    _name = 'gdm.supplier'
+    _table = 'gdm_supplier'
     _description = 'Goods suppliers'
     _columns = {
         'name': fields.many2one('res.users', help='Supplier that will supplier the goods', select=True, copy=False),
@@ -21,7 +22,6 @@ class goods_supplier(osv.osv):
         'email': fields.char('Email', help='Email Id of supplier', select=True),
         'website': fields.char('Website', help='Supplier`s website', select=True),
         'mobile': fields.char('Mobile No', help='Supplier`s mobile number', select=True),
-        'fax': fields.char('Fax', help='Supplier`s Fax', select=True)
 
-    }
+      }
 
