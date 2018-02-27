826b0437c22eccbc8c57bf5bcd638df1e6e7aa53
luckysher <luckyshera95@gmail.com>
2018-02-27 09:51:11 +0530

    added selection for pay status

--- models/models.py
+++ models/models.py
@@ -59,7 +59,7 @@ class invoice(osv.osv):
                                         help='Fiscal position', select=True),
         'source_doc': fields.char('Source document', help='', select=True),
         'Supplier Invoice number': fields.char('Invoice number for the supplier', help='', select=True),
-        'pays_status': fields.char('Payment Status', help='', select=True),
+        'pays_status': fields.selection([('',''), ('done', 'Done'), ('pending', 'Pending')], 'Payment Status', help='', select=True),
         'invoice_date': fields.date('Invoice date', help="Date of the request"),
         'due_date': fields.date('Due date', help="Due Date for the payment")
     }
